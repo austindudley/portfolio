f49eec15f77bd91e600afc3163de83ec5486d087
austindudley <austindudley10@gmail.com>
2024-09-30 20:37:08 -0400

    build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Navbar from "./pages/Navbar";
 import About from "./pages/About";
